57ff924ef3cdea969d7c0d0b024cc38a8ec738d1
ChristianRkx <73137784@continental.edu.pe>
2025-09-30 22:06:04 -0500

    Avance

--- api-rest.js
+++ api-rest.js
@@ -1,17 +1,13 @@
-
-
-const claveApi ='d8a299ca3a3e4b2cabe145258253009';
+const claveApi = "d8a299ca3a3e4b2cabe145258253009";
 const idioma = 'es';
 const ciudad = 'Huancayo';
 
-
-const apiClimaActual = `https://api.weatherapi.com/v1/current.json?
-q=${ciudad}&lang=${idioma}&key=${claveApi}`;
-
+const apiClimaActual = `https://api.weatherapi.com/v1/current.json?q=${ciudad}&lang=${idioma}&key=${claveApi}`;
 
 const response = await fetch(apiClimaActual);
 let data = await response.json();
-
-
-
+    
 console.log(data);
+console.log(data.location);
+console.log(data.location.localtime);
+console.log(data.current.condition);
--- codigo.js
+++ codigo.js
@@ -1,36 +1,27 @@
-
-
-const claveApi ='d8a299ca3a3e4b2cabe145258253009';
-const idioma = 'es';
+const claveApi = "d8a299ca3a3e4b2cabe145258253009"; 
 const inpCiudad = document.getElementById('input-ciudad');
 
-
 async function obtenerClima() {
-    
     const ciudad = inpCiudad.value;
-    if(!ciudad){
-        alert('Por favor, Ingrese una ciudad')
+
+    if (!ciudad) {
+        alert('Por favor, ingresa una ciudad');
         return;
     }
 
+    const apiClimaActual = `https://api.weatherapi.com/v1/current.json?q=${ciudad}&lang=${idioma}&key=${claveApi}`;
 
-    const apiClimaActual = `https://api.weatherapi.com/v1/current.json?
-q=${ciudad}&lang=${idioma}&key=${claveApi}`;
+    const response = await fetch(apiClimaActual);
+    const data = await response.json();
 
-const response = await fetch(apiClimaActual);
-const data = await response.json();
-
-mostrarClima(data);
-
+    mostrarClima(data);
 }
-3
 
-function mostrarClima(data){
-    Document.querySelector('.clima-icono').src = data.current.condition.icon;
-    Document.querySelector('.clima-texto').innerHTML = data.current.condition.text;
-    Document.querySelector('.temp').innerHTML = data.current.temp_c + '°C';
-    Document.querySelector('.ciudad').innerHTML = data.current.name;
-    Document.querySelector('.humedad').innerHTML = data.current.humidity + '%';
-    Document.querySelector('.viento').innerHTML = data.current.wind_kph + 'km/h';
-
+function mostrarClima(data) {
+    document.querySelector('.clima-icono').src = data.current.condition.icon;
+    document.querySelector('.clima-texto').innerHTML = data.current.condition.text;
+    document.querySelector('.temp').innerHTML = data.current.temp_c + 'ºC';
+    document.querySelector('.ciudad').innerHTML = data.location.name;
+    document.querySelector('.humedad').innerHTML = data.current.humidity + '%';
+    document.querySelector('.viento').innerHTML = data.current.wind_kph + ' km/h';
 }
